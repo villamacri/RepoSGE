6715ccb6edeaef7a93e89b6de3f6e4acf9cc4a77
villamacri <villalba.macri24@triana.salesianos.edu>
2026-01-20 17:03:26 +0100

    ejercicio09hecho

--- Tema03/EjerciciosListado02/ejercicio09.py
+++ Tema03/EjerciciosListado02/ejercicio09.py
@@ -1,63 +1,60 @@
 class Droid:
-    def __init__(self, modelo, vueltas, kmPorVuelta, totalBajas, derribos):
-        self.modelo=modelo
-        pass
+    def __init__(self, modelo):
+        self.modelo = modelo
     
-    def calcularDistanciaRecorr():
-        pass
+    def calcularDistanciaRecorr(self):
+        return 0
     
-    def calcularBajasConfirmadas():
-        pass
+    def calcularBajasConfirmadas(self):
+        return 0
     
 class ProtocolDroid(Droid):
     def __init__(self, modelo, vueltas, kmPorVuelta):
         super().__init__(modelo)
-        self.vueltas=vueltas
-        self.kmPorVuelta=kmPorVuelta
+        self.vueltas = vueltas
+        self.kmPorVuelta = kmPorVuelta
         
-    def calcularDistanciaRecorr(vueltas, kmPorVuelta):
-        return kmPorVuelta*vueltas
+    def calcularDistanciaRecorr(self):
+        return self.kmPorVuelta * self.vueltas
 
 class AstromechDroid(Droid):
     def __init__(self, modelo, totalBajas, derribos):
         super().__init__(modelo)
-        self.totalBajas=totalBajas
-        self.derribos=derribos
+        self.totalBajas = totalBajas
+        self.derribos = derribos
         
-    def calcularBajasConfirmadas(totalBajas, derribos):
-        return totalBajas-derribos
+    def calcularBajasConfirmadas(self):
+        return self.totalBajas - self.derribos
     
     
 class SuperDroid(AstromechDroid, ProtocolDroid):
     def __init__(self, modelo, vueltas, kmPorVuelta, totalBajas, derribos):
-        super().__init__(modelo)
-        self.vueltas=vueltas
-        self.kmPorVuelta=kmPorVuelta
-        self.totalBajas=totalBajas
-        self.derribos=derribos
-    
-    def calcularBajasConfirmadas(totalBajas, derribos):
-        return super().calcularBajasConfirmadas(derribos)
-    
-    def calcularDistanciaRecorr(vueltas, kmPorVuelta):
-        return super().calcularDistanciaRecorr(kmPorVuelta)
+        Droid.__init__(self, modelo)
+        self.vueltas = vueltas
+        self.kmPorVuelta = kmPorVuelta
+        self.totalBajas = totalBajas
+        self.derribos = derribos
     
 class HyperDroid(ProtocolDroid, AstromechDroid):
     def __init__(self, modelo, vueltas, kmPorVuelta, totalBajas, derribos):
-        super().__init__(modelo)
-        self.vueltas=vueltas
-        self.kmPorVuelta=kmPorVuelta
-        self.totalBajas=totalBajas
-        self.derribos=derribos
-        
-    def calcularDistanciaRecorr(vueltas, kmPorVuelta):
-        return super().calcularDistanciaRecorr(kmPorVuelta)
-    
-    def calcularBajasConfirmadas(totalBajas, derribos):
-        return super().calcularBajasConfirmadas(derribos)
+        Droid.__init__(self, modelo)
+        self.vueltas = vueltas
+        self.kmPorVuelta = kmPorVuelta
+        self.totalBajas = totalBajas
+        self.derribos = derribos
     
 
-    
-droides=[
-    Droid()
-]        
+droides = [
+    ProtocolDroid("C-3PO", 5, 10),
+    AstromechDroid("R2-D2", 10, 2),
+    SuperDroid("IG-88", 10, 5, 20, 5),
+    HyperDroid("BB-8", 20, 2, 5, 1)
+]
+
+print(f"{'Modelo':<10} | {'Distancia':<10} | {'Bajas':<10}")
+print("-" * 36)
+
+for droid in droides:
+    dist = droid.calcularDistanciaRecorr()
+    bajas = droid.calcularBajasConfirmadas()
+    print(f"{droid.modelo:<10} | {dist:<10} | {bajas:<10}")
